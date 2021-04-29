1037171d419d84faf42d3c022a6f2091172e9f46
Marco Schicker <test@marcoschicker.de>
2021-04-29 23:06:03 +0200

    no edx_visual rel_year as column incl. correction timstamp-columns created in edx_try and validation boxplot rating be rel_year

--- code.R
+++ code.R
@@ -70,16 +70,33 @@ rm(dl, ratings, movies, test_index, temp, movielens, removed)
 try_index <- createDataPartition(y = edx$rating, times = 1, p = 0.001, list = FALSE)
 edx_try <- edx[try_index,]
 dim(edx_try)
-### data exploration
-summary(edx)
-str(edx)
-edx_desc <- describe(edx)
 
-# mutate data (REMOVE NA´S, rating as.factor, convert timestamp 
-#edx_clean <- edx_try %>% 
-#mutate(rating = ifelse(is.na(rating), median(rating, na.rm = TRUE), rating),
-#         timestamp = as_datetime(timestamp)# convert timestamp to a date_time object
-#  )
+
+### data exploration
+summary(edx_try)
+edx_desc <- describe(edx_try)
+edx_desc
+
+         
+
+## extract release year from title column to create a new predictor for edx and validation
+## extract year, month and day from timestamp
+edx_try <- edx_try %>% 
+  mutate(rel_year = str_extract(title, "(\\d{4})"), # create rel_year base on the title-entry
+         rel_year = as.numeric(str_replace_all(rel_year,"(|)", "")), # remove parantheses and convert to number
+         rel_year = ifelse(rel_year>2020, 1994, ifelse(rel_year < 1900, 1994,rel_year) ),
+         ts_year = year(as_datetime(timestamp)),
+         ts_month = month(as_datetime(timestamp), label = FALSE),
+         ts_day = wday(as_datetime(timestamp), label = FALSE)
+        )
+
+validation <- validation %>%
+  mutate(rel_year = str_extract(title, "(\\d{4})"),
+         rel_year = as.numeric(str_replace_all(rel_year,"(|)", "")),
+         ts_year = year(as_datetime(timestamp)),
+         ts_month = month(as_datetime(timestamp), label = FALSE),
+         ts_day = wday(as_datetime(timestamp), label = FALSE)
+         )
 
 
 #look for duplicate entries
@@ -91,40 +108,44 @@ problems
 d <- data.frame(userId = edx_try$userId,
                 movieId = edx_try$movieId,
                 rating = edx_try$rating,
-                timestamp = edx_try$timestamp)
+                timestamp = edx_try$timestamp,
+                rel_year = edx_try$rel_year,
+                ts_year = edx_try$ts_year,
+                ts_month = edx_try$ts_month,
+                ts_day = edx_try$ts_day
+                )
 corrplot(cor(d), method = "number")
 
-
-
-### data visualization
 genres = c("Drama", "Comedy", "Thriller", "Romance")
 sapply(genres, function(g) {
   sum(str_detect(edx$genres, g))
 })
 
-edx_visual <- edx_try %>% mutate(
-  year = year(as_datetime(timestamp)),
-  month = month(as_datetime(timestamp), label=TRUE),
-  day = wday(as_datetime(timestamp), label = TRUE))
+### data visualization
 
 
-##histogram rating
-edx_visual %>% ggplot(aes(rating))+
+##histogram rating overall
+edx_try %>% ggplot(aes(rating))+
   geom_histogram(binwidth = 0.5)
 
-r<- mean(edx_visual$rating)
+r<- mean(edx_try$rating)
+
+
+##histograms by filter ()
+
+
 
 ##avg rating by year
-edx_visual %>%
-  group_by(year)%>%
+edx_try %>%
+  group_by(ts_year)%>%
   summarize(avg_rating = mean(rating))%>%
-  ggplot(aes(x=year, y=avg_rating))+
+  ggplot(aes(x=ts_year, y=avg_rating))+
   geom_point()+
   geom_line()
 
 ##boxplot ratings by year
-edx_visual %>%
-  ggplot(aes(x=year, y=rating, group=year))+
+edx_try %>%
+  ggplot(aes(x=ts_year, y=rating, group=ts_year))+
   geom_boxplot()+
  # geom_jitter(shape=21, alpha=0.5)+
   stat_summary(fun=mean, geom="point", shape=15, size=4, col=2)+
@@ -132,8 +153,8 @@ edx_visual %>%
 
 
 ##boxplot ratings by month
-edx_visual %>%
-  ggplot(aes(x=month, y=rating, group=month))+
+edx_try %>%
+  ggplot(aes(x=ts_month, y=rating, group=ts_month))+
   geom_boxplot()+
   # geom_jitter(shape=21, alpha=0.5)+
   stat_summary(fun=mean, geom="point", shape=15, size=4, col=2)+
@@ -141,14 +162,20 @@ edx_visual %>%
 
     
 ##boxplot ratings by weekday
-edx_visual %>%
-  ggplot(aes(x=day, y=rating, group=day))+
+edx_try %>%
+  ggplot(aes(x=ts_day, y=rating, group=ts_day))+
   geom_boxplot()+
   #geom_jitter(shape=21, alpha=0.5)+
   stat_summary(fun=mean, geom="point", shape=15, size=4, col=2)+
   coord_flip()
 
-
+##boxplot ratings by release year of the movie
+edx_try %>%
+  ggplot(aes(x=rel_year, y=rating, group=rel_year))+
+  geom_boxplot()+
+  #geom_jitter(shape=21, alpha=0.5)+
+  stat_summary(fun=mean, geom="point", shape=15, size=4, col=2)+
+  coord_flip()
 
 
 
@@ -246,8 +273,6 @@ predicted_ratings <- edx_test %>%
   .$pred
 
 predicted_ratings <- ifelse(!is.na(predicted_ratings),predicted_ratings, mu+b_i)
-#predicted_ratings <- round(predicted_ratings*2,1)/2 #round the result to the closest 0.5-step
-
 
 model_2.2_rmse <- RMSE(edx_test$rating, predicted_ratings)
 
@@ -269,9 +294,119 @@ edx_train %>% group_by(genres) %>%
   theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
 
+genre_avgs <- edx_test %>% 
+  left_join(movie_avgs, by='movieId') %>%
+  left_join(user_avgs, by='userId') %>%
+  group_by(genres) %>%
+ # summarize(n = n(), avg = mean(rating), se = sd(rating)/sqrt(n())) %>%
+  #filter(n >= 100) %>%
+  summarize(b_g = mean(rating - mu - b_i - b_u)) 
+
+pred_ratings <- edx_test %>% 
+  left_join(movie_avgs, by='movieId') %>%
+  left_join(user_avgs, by='userId') %>%
+  left_join(genre_avgs, by='genres')%>%
+  mutate(pred = mu + b_i + b_u + b_g) %>%
+  .$pred
+
+pred_ratings <- ifelse(!is.na(pred_ratings),pred_ratings, mu+b_i+b_u)
+
+model_2.3_rmse <- RMSE(edx_test$rating, pred_ratings)
+
+rmse_results <- bind_rows(rmse_results,
+                          data_frame(method="linear movieId + userId + genre",
+                                     RMSE = model_2.3_rmse ))
+rmse_results %>% knitr::kable()
 
 
 
+####MODEL2.4 - REGULARIZED LINEAR MODEL####
+lambda <- 3
+mu <- mean(edx_train$rating)
+movie_reg_avgs <- edx_train %>% 
+  group_by(movieId) %>% 
+  summarize(b_i = sum(rating - mu)/(n()+lambda), n_i = n()) 
+
+data_frame(original = movie_avgs$b_i, 
+           regularlized = movie_reg_avgs$b_i, 
+           n = movie_reg_avgs$n_i) %>%
+  ggplot(aes(original, regularlized, size=sqrt(n))) + 
+  geom_point(shape=1, alpha=0.5)
+
+edx_train %>%
+  dplyr::count(movieId) %>% 
+  left_join(movie_reg_avgs) %>%
+  left_join(movie_titles, by="movieId") %>%
+  arrange(desc(b_i)) %>% 
+  select(title, b_i, n) %>% 
+  slice(1:10) %>% 
+  knitr::kable()
+
+edx_train %>%
+  dplyr::count(movieId) %>% 
+  left_join(movie_reg_avgs) %>%
+  left_join(movie_titles, by="movieId") %>%
+  arrange(b_i) %>% 
+  select(title, b_i, n) %>% 
+  slice(1:10) %>% 
+  knitr::kable()
+
+predicted_ratings <- edx_test %>% 
+  left_join(movie_reg_avgs, by='movieId') %>%
+  mutate(pred = mu + b_i) %>%
+  .$pred
+
+model_3_rmse <- RMSE(predicted_ratings, edx_test$rating)
+rmse_results <- bind_rows(rmse_results,
+                          data_frame(method="Regularized Movie Effect Model",  
+                                     RMSE = model_3_rmse ))
+rmse_results %>% knitr::kable()
+
+####use cross validation to find the best lambda
+lambdas <- seq(0, 10, 0.25)
+mu <- mean(train_set$rating)
+just_the_sum <- train_set %>% 
+  group_by(movieId) %>% 
+  summarize(s = sum(rating - mu), n_i = n())
+rmses <- sapply(lambdas, function(l){
+  predicted_ratings <- test_set %>% 
+    left_join(just_the_sum, by='movieId') %>% 
+    mutate(b_i = s/(n_i+l)) %>%
+    mutate(pred = mu + b_i) %>%
+    .$pred
+  return(RMSE(predicted_ratings, test_set$rating))
+})
+qplot(lambdas, rmses)  
+lambdas[which.min(rmses)]
+
+lambdas <- seq(0, 10, 0.25)
+rmses <- sapply(lambdas, function(l){
+  mu <- mean(train_set$rating)
+  b_i <- train_set %>%
+    group_by(movieId) %>%
+    summarize(b_i = sum(rating - mu)/(n()+l))
+  b_u <- train_set %>% 
+    left_join(b_i, by="movieId") %>%
+    group_by(userId) %>%
+    summarize(b_u = sum(rating - b_i - mu)/(n()+l))
+  predicted_ratings <- 
+    test_set %>% 
+    left_join(b_i, by = "movieId") %>%
+    left_join(b_u, by = "userId") %>%
+    mutate(pred = mu + b_i + b_u) %>%
+    .$pred
+  return(RMSE(predicted_ratings, test_set$rating))
+})
+
+qplot(lambdas, rmses)  
+
+lambda <- lambdas[which.min(rmses)]
+lambda
+
+rmse_results <- bind_rows(rmse_results,
+                          data_frame(method="Regularized Movie + User Effect Model",  
+                                     RMSE = min(rmses)))
+rmse_results %>% knitr::kable()
 
 ####MODEL 3.1 - knn####
 
@@ -329,13 +464,11 @@ rmse_results <- bind_rows(rmse_results,
                           data_frame(method="Decision tree",
                                      RMSE = model_4_rmse ))
 ####MODEL 5 - LOESS####
-train_rf <- train(rating ~., 
+train_loess <- train(rating ~userId+movieId+as_datetime(timestamp), 
                   method = "loess", 
-                  tuneGrid = data.frame(mtry = seq(1:7)),
-                  ntree = 100,
                   data = edx_train)
 train_rf$bestTune
-model5_predict <- predict(train_rf, test_set)
+model5_predict <- predict(train_loess, test_set)
 
 model_5_rmse <- RMSE(edx_test$rating, model5_predict)
 
@@ -343,13 +476,13 @@ rmse_results <- bind_rows(rmse_results,
                           data_frame(method="LOESS",
                                      RMSE = model_5_rmse ))
 ####MODEL 6 - Random Forest####
-train_rf <- train(rating ~., 
+train_rf <- train(rating ~userId+movieId+as_datetime(timestamp), 
                   method = "rf", 
                   tuneGrid = data.frame(mtry = seq(1:7)),
-                  ntree = 100,
+                  ntree = 5,
                   data = edx_train)
 train_rf$bestTune
-model6_predict <- predict(train_rf, test_set)
+model6_predict <- predict(train_rf, edx_test)
 
 
 model_6_rmse <- RMSE(edx_test$rating, model6_predict)
@@ -357,6 +490,23 @@ model_6_rmse <- RMSE(edx_test$rating, model6_predict)
 rmse_results <- bind_rows(rmse_results,
                           data_frame(method="random forest",
                                      RMSE = model_6_rmse ))
+rmse_results %>% knitr::kable()
+#####MODEL 7 - GLM####
+
+train_glm <- train(rating ~userId+movieId+as_datetime(timestamp), 
+                  method = "glm", 
+                  data = edx_train)
+train_glm$bestTune
+model7_predict <- predict(train_glm, edx_test)
+
+
+model_7_rmse <- RMSE(edx_test$rating, model7_predict)
+
+rmse_results <- bind_rows(rmse_results,
+                          data_frame(method="GLM",
+                                     RMSE = model_7_rmse ))
+rmse_results %>% knitr::kable()
+
 #####REGULARIZATION######
 
 ###watch out: no regularization if values are not centered around 0 ==> first subtract the overall mean
